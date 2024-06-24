2b39bad4656dda930292c07a5dbb93b5b2d599ed
miss-gif <ddc.akari@gmail.com>
2024-06-24 21:06:19 +0900

    Init : 초기 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import RootLayout from "./components/layout/RootLayout";
+import RestaurantsPage from "./pages/RestaurantsPage.jsx";
+import RestaurantDetailPage from "./pages/RestaurantDetailPage.jsx";
+import CartPage from "./pages/CartPage.jsx";
+import CheckoutPage from "./pages/CheckoutPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import AuthPage from "./pages/AuthPage.jsx";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/restaurants" element={<RestaurantsPage />} />
+          <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
 
 export default App;
