c8566c4c8182014958abd1162b038bde77826051
miss-gif <ddc.akari@gmail.com>
2024-06-29 21:31:58 +0900

    Feat : 스크롤 상단 이동 버튼 기능

--- src/components/common/ToTop.jsx
+++ src/components/common/ToTop.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MdArrowUpward } from "react-icons/md";
 
 const ToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    const scrollPosition = window.pageYOffset;
+    setIsVisible(scrollPosition > 0);
+  };
+
+  const clickTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="to-top">
-      <MdArrowUpward />
-    </div>
+    isVisible && (
+      <div className="to-top" onClick={clickTop}>
+        <MdArrowUpward />
+      </div>
+    )
   );
 };
 
