c146f50636248fe40c3e4e09f3024fcf5b21bcb0
miss-gif <svx327@naver.com>
2024-07-02 19:30:57 +0900

    Test : 모달 컴포넌트 테스트 코드

--- src/components/common/ModalComponent.jsx
+++ src/components/common/ModalComponent.jsx
@@ -17,67 +17,41 @@ const ModalComponent = ({
   onCheck,
 }) => {
   return (
-    <>
-      <Modal
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-        contentLabel={title}
-        style={modalStyles}
-      >
-        <Title>{title}</Title>
-        <Content>{content}</Content>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel={title}
+      style={modalStyles}
+    >
+      <Title>{title}</Title>
+      <Content>{content}</Content>
 
-        {type === "confirm" && (
-          <ButtonGroup>
-            <Button onClick={onConfirm}>Yes</Button>
-            <Button onClick={onCancel}>No</Button>
-          </ButtonGroup>
-        )}
+      {type === "confirm" && (
+        <ButtonGroup>
+          <Button onClick={onConfirm}>Yes</Button>
+          <Button onClick={onCancel}>No</Button>
+        </ButtonGroup>
+      )}
 
-        {type === "checkbox" && (
-          <>
-            <CheckboxGroup>
-              {options.map((option, index) => (
-                <CheckboxLabel key={index}>
-                  <input
-                    type="checkbox"
-                    checked={option.checked}
-                    onChange={() => onCheck(index)}
-                  />
-                  {option.label}
-                </CheckboxLabel>
-              ))}
-            </CheckboxGroup>
-            <Button onClick={onConfirm}>Confirm</Button>
-          </>
-        )}
-      </Modal>
-    </>
+      {type === "checkbox" && (
+        <>
+          <CheckboxGroup>
+            {options.map((option, index) => (
+              <CheckboxLabel key={index}>
+                <Checkbox
+                  type="checkbox"
+                  checked={option.checked}
+                  onChange={() => onCheck(index)}
+                />
+                {option.label}
+              </CheckboxLabel>
+            ))}
+          </CheckboxGroup>
+          <Button onClick={onConfirm}>Confirm</Button>
+        </>
+      )}
+    </Modal>
   );
-};
-
-// prop에 대한 타입 검증
-ModalComponent.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  onRequestClose: PropTypes.func.isRequired,
-  type: PropTypes.oneOf(["confirm", "checkbox"]).isRequired,
-  title: PropTypes.string.isRequired,
-  content: PropTypes.node.isRequired,
-  onConfirm: PropTypes.func.isRequired,
-  onCancel: PropTypes.func,
-  options: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string.isRequired,
-      checked: PropTypes.bool.isRequired,
-    }),
-  ),
-  onCheck: PropTypes.func,
-};
-
-ModalComponent.defaultProps = {
-  onCancel: () => {},
-  options: [],
-  onCheck: () => {},
 };
 
 export default ModalComponent;
@@ -94,6 +68,7 @@ const modalStyles = {
     padding: "20px",
     width: "400px",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    backgroundColor: "#fff", // 추가
   },
 };
 
@@ -136,9 +111,40 @@ const CheckboxLabel = styled.label`
   align-items: center;
   margin-bottom: 10px;
   color: #555;
-  input {
-    width: 50px;
-    height: 50px;
-    border-color: orange;
+`;
+
+const Checkbox = styled.input`
+  margin-right: 10px;
+  width: 20px; // 기본 체크박스 크기로 조정
+  height: 20px; // 기본 체크박스 크기로 조정
+  border: 1px solid #ccc; // 기본 테두리 색상 설정
+  border-radius: 3px; // 둥근 모서리 설정
+  &:checked {
+    background-color: #007bff; // 체크된 상태의 배경색 설정
+    border-color: #007bff; // 체크된 상태의 테두리 색상 설정
   }
 `;
+
+// prop에 대한 타입 검증
+ModalComponent.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onRequestClose: PropTypes.func.isRequired,
+  type: PropTypes.oneOf(["confirm", "checkbox"]).isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.node.isRequired,
+  onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      checked: PropTypes.bool.isRequired,
+    }),
+  ),
+  onCheck: PropTypes.func,
+};
+
+ModalComponent.defaultProps = {
+  onCancel: () => {},
+  options: [],
+  onCheck: () => {},
+};
