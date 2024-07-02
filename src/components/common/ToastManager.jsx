36e2e473c267be2f390006155d95d45b19183811
miss-gif <svx327@naver.com>
2024-07-02 19:07:22 +0900

    Feat : 메시지 알림 토스트 기능

---
+++ src/components/common/ToastManager.jsx
@@ -0,0 +1,39 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const notify = ({ type, text }) => {
+  switch (type) {
+    case "default":
+      toast(text);
+      break;
+    case "success":
+      toast.success(text);
+      break;
+    case "warning":
+      toast.warning(text);
+      break;
+    case "error":
+      toast.error(text);
+      break;
+  }
+};
+
+const ToastManager = () => {
+  return (
+    <ToastContainer
+      position="bottom-right"
+      autoClose={1000}
+      hideProgressBar={true} // 남은 시간 표시 off
+      newestOnTop={false} // 쌓임 순서
+      closeOnClick // 클릭 시 닫힘
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  );
+};
+
+export default ToastManager;
+export { toast };
