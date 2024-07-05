7fcabf1c25fc956813869fa079558b26fb77f15b
miss-gif <svx327@naver.com>
2024-07-05 16:47:33 +0900

    Fix : payment-page 수정

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -52,21 +52,29 @@ const PaymentPage = () => {
             <div className="payment-page__input-wrap">
               <h3 className="payment-page__subtitle">결제수단 선택</h3>
               <div className="payment-page__payment-method">
-                <div className="payment-page__mobile-payment">
-                  <button className="payment-page__button btn--active">
-                    네이버페이
-                  </button>
-                  <button className="payment-page__button btn--default">
-                    카카오페이
-                  </button>
+                <div className="payment-wrap">
+                  <h4>
+                    주문이요<span>웹에서 미리 결제</span>
+                  </h4>
+                  <div className="payment-page__mobile-payment">
+                    <button className="payment-page__button btn--active">
+                      네이버페이
+                    </button>
+                    <button className="payment-page__button btn--default">
+                      카카오페이
+                    </button>
+                  </div>
                 </div>
-                <div className="payment-page__onsite-payment">
-                  <button className="payment-page__button btn--default">
-                    신용카드
-                  </button>
-                  <button className="payment-page__button btn--default">
-                    현금
-                  </button>
+                <div className="payment-wrap">
+                  <h4>현장 결제</h4>
+                  <div className="payment-page__onsite-payment">
+                    <button className="payment-page__button btn--default">
+                      신용카드
+                    </button>
+                    <button className="payment-page__button btn--default">
+                      현금
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
