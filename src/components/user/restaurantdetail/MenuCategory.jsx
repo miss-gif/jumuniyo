19266fe1a751a80d4962e2e1773efc26cbf963d2
miss-gif <ddc.akari@gmail.com>
2024-07-08 23:24:00 +0900

    Feat : 사업장 메뉴 아코디언 기능

--- src/components/user/restaurantdetail/MenuCategory.jsx
+++ src/components/user/restaurantdetail/MenuCategory.jsx
@@ -1,47 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const MenuCategory = () => {
+  const [isOpen, setIsOpen] = useState(true); // 메뉴 카테고리의 펼침 상태
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const menuItems = [
+    {
+      name: "한마리 ＋ 순살치킨",
+      description: "국내산 하림닭 ／ 가심비 두 배 세트!",
+      price: "23,000원",
+      image: "https://picsum.photos/100/",
+    },
+    {
+      name: "한마리 ＋ 순살치킨",
+      description: "국내산 하림닭 ／ 가심비 두 배 세트!",
+      price: "23,000원",
+      image: "https://picsum.photos/100/",
+    },
+    {
+      name: "한마리 ＋ 순살치킨",
+      description: "국내산 하림닭 ／ 가심비 두 배 세트!",
+      price: "23,000원",
+      image: "https://picsum.photos/100/",
+    },
+  ];
+
   return (
     <div className="menu-category">
-      <h4 className="menu-category__title">인기메뉴</h4>
-      <ul className="menu-category__list">
-        <li className="menu-category__item">
-          <div className="menu-category__text">
-            <div className="menu-category__name">한마리 ＋ 순살치킨</div>
-            <div className="menu-category__description">
-              국내산 하림닭 ／ 가심비 두 배 세트!
-            </div>
-            <div className="menu-category__price">23,000원</div>
-          </div>
-          <div className="menu-category__image">
-            <img src="https://picsum.photos/100/" alt="" />
-          </div>
-        </li>
-        <li className="menu-category__item">
-          <div className="menu-category__text">
-            <div className="menu-category__name">한마리 ＋ 순살치킨</div>
-            <div className="menu-category__description">
-              국내산 하림닭 ／ 가심비 두 배 세트!
-            </div>
-            <div className="menu-category__price">23,000원</div>
-          </div>
-          <div className="menu-category__image">
-            <img src="https://picsum.photos/100/" alt="" />
-          </div>
-        </li>
-        <li className="menu-category__item">
-          <div className="menu-category__text">
-            <div className="menu-category__name">한마리 ＋ 순살치킨</div>
-            <div className="menu-category__description">
-              국내산 하림닭 ／ 가심비 두 배 세트!
-            </div>
-            <div className="menu-category__price">23,000원</div>
-          </div>
-          <div className="menu-category__image">
-            <img src="https://picsum.photos/100/" alt="" />
-          </div>
-        </li>
-      </ul>
+      <div className="toggle-category" onClick={toggleMenu}>
+        <h4 className="menu-category__title">인기메뉴</h4>
+        <div className="toggle-category-icon">
+          {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </div>
+      </div>
+      {isOpen && (
+        <ul className="menu-category__list">
+          {menuItems.map((item, index) => (
+            <li key={index} className="menu-category__item">
+              <div className="menu-category__text">
+                <div className="menu-category__name">{item.name}</div>
+                <div className="menu-category__description">
+                  {item.description}
+                </div>
+                <div className="menu-category__price">{item.price}</div>
+              </div>
+              <div className="menu-category__image">
+                <img src={item.image} alt={item.name} />
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
