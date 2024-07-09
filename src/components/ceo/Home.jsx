9b990f901d44787e9ae24126463f26c51a4c3edd
miss-gif <svx327@naver.com>
2024-07-09 18:24:54 +0900

    Docs : 사업자 홈 페이지 작업중 및 사업자 css 수정

--- src/components/ceo/Home.jsx
+++ src/components/ceo/Home.jsx
@@ -1,103 +1,162 @@
 import React from "react";
 
 const Home = () => {
+  const today = new Date();
+  console.log(today);
   return (
-    <div className="ceo-home">
-      <div className="home-tab">홈</div>
-      <div className="order-body">
-        <div className="confirmedList">
-          <div className="confirmedList-tab">접수된 주문</div>
-          {/* 반복 */}
-          <div className="oneOrder">
-            <div className="orderedTime">13:22</div>
-            <div className="orderInfo">
-              <div className="orderAmount">
-                <div className="orderNumber">[메뉴 4개]</div>
-                <div className="orderPrice">31,800</div>
-                <div className="payMethod">후불</div>
-              </div>
-              <div className="orderedMenu">
-                {/* 반복 */}
-                <div className="menu-name">김치찌개</div>
-                <div className="menu-amount">1개</div>
-              </div>
-              <div className="orderedAddress">
-                서울 송파구 방이동 44-2 장은빌딩 9층
-              </div>
-              <div className="requested">단무지 많이 주고 일회용품 ㄴㄴ</div>
-            </div>
-            <div className="orderStatus">
-              <div className="orderLeftTime">12분</div>
-              <div className="orderProcess">배달중</div>
-            </div>
-          </div>
-          <div className="oneOrder">
-            <div className="orderedTime">13:22</div>
-            <div className="orderInfo">
-              <div className="orderAmount">
-                <div className="orderNumber">[메뉴 4개]</div>
-                <div className="orderPrice">31,800</div>
-                <div className="payMethod">후불</div>
-              </div>
-              <div className="orderedMenu">
-                {/* 반복 */}
-                <div className="menu-name">김치찌개</div>
-                <div className="menu-amount">1개</div>
-              </div>
-              <div className="orderedAddress">
-                서울 송파구 방이동 44-2 장은빌딩 9층
-              </div>
-              <div className="requested">단무지 많이 주고 일회용품 ㄴㄴ</div>
-            </div>
-            <div className="orderStatus">
-              <div className="orderLeftTime">12분</div>
-              <div className="orderProcess">배달중</div>
-            </div>
-          </div>
-        </div>
-        <div className="orderedList">
-          {/* 반복 */}
-          <div className="oneOrder">
-            <div className="AnOrderHead">새 주문</div>
-            <div className="AnOrderBody">
-              <div className="AnOrderLeft">
-                <div className="requested">
-                  요청사항
-                  <div className="requestedtoStore">맛있게 해주세요</div>
-                  <div className="requestedtoRider">빨리와주세요</div>
-                </div>
-                <div className="orderedMenu">
-                  주문내역
-                  <div className="menuName">양념치킨</div>
-                  <div className="menuAmount">1</div>
-                  <div className="menuPrice">3,000</div>
-                </div>
-              </div>
-              <div className="AnOrderRight">
-                <div className="AnOrderInf">
-                  배달주소
-                  <div className="orderAddress">
-                    경기도 화성시 이성로 대명아파트 102동 906호
+    <>
+      <div className="ceo-home">
+        <div className="order-body">
+          <div className="left">
+            <div className="confirmedList">
+              <div className="confirmedList-tab">접수된 주문</div>
+              {/* 반복 */}
+              <div className="oneOrder">
+                <div className="orderedTime">13:22</div>
+                <div className="orderInfo">
+                  <div className="orderAmount">
+                    <div className="orderNumber">[메뉴 4개]</div>
+                    <div className="orderPrice">31,800</div>
+                    <div className="payMethod">후불</div>
                   </div>
-                  고객연락처
-                  <div className="orderCallNumber">010-1111-2222 </div>
-                  주문번호
-                  <div className="orderCallNumber">PK012345143516 </div>
-                  주문시간
-                  <div className="orderCallNumber">13:54 </div>
+                  <div className="orderedMenu">
+                    {/* 반복 */}
+                    <div className="menu-name">김치찌개</div>
+                    <div className="menu-amount">1개</div>
+                  </div>
+                  <div className="orderedAddress">
+                    서울 송파구 방이동 44-2 장은빌딩 9층
+                  </div>
+                  <div className="requested">
+                    단무지 많이 주고 일회용품 ㄴㄴ
+                  </div>
                 </div>
-                <div className="acceptOrRefuse">
-                  <button className="btn">수락하기</button>
-                  <button className="btn">취소하기</button>
+                <div className="orderStatus">
+                  <div className="orderLeftTime">12분</div>
+                  <div className="orderProcess">배달중</div>
                 </div>
+              </div>
+              <div className="oneOrder">
+                <div className="orderedTime">13:22</div>
+                <div className="orderInfo">
+                  <div className="orderAmount">
+                    <div className="orderNumber">[메뉴 4개]</div>
+                    <div className="orderPrice">31,800</div>
+                    <div className="payMethod">후불</div>
+                  </div>
+                  <div className="orderedMenu">
+                    {/* 반복 */}
+                    <div className="menu-name">김치찌개</div>
+                    <div className="menu-amount">1개</div>
+                  </div>
+                  <div className="orderedAddress">
+                    서울 송파구 방이동 44-2 장은빌딩 9층
+                  </div>
+                  <div className="requested">
+                    단무지 많이 주고 일회용품 ㄴㄴ
+                  </div>
+                </div>
+                <div className="orderStatus">
+                  <div className="orderLeftTime">12분</div>
+                  <div className="orderProcess">배달중</div>
+                </div>
+              </div>
+            </div>
+            <div className="allSalesOntheDay">
+              <h2 className="title">총매출</h2>
+              <div className="allSalesBody">
+                <h2>
+                  {today.getMonth() + 1}월 {today.getDate()}일
+                </h2>
+                <div className="tabAndValue">
+                  <div className="allSalesTab">
+                    <h2 className="sales">판매 금액</h2>
+                    <h2 className="salesNumber">판매 수</h2>
+                  </div>
+                  <div className="allSalesValue">
+                    <h2 className="sales">123,000</h2>
+                    <h2 className="salesNumber">7건</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button className="btn">기준시간 정하기</button>
+          </div>
+          <div className="orderedList">
+            {/* 반복 */}
+            <div className="oneOrder">
+              <div className="AnOrderHead">새 주문</div>
+              <div className="AnOrderBody">
+                <div className="AnOrderLeft">
+                  <div className="requested">
+                    <h3>요청사항</h3>
+                    <div className="requestedabout">
+                      <div className="requestedto">
+                        <h4>가게 </h4>
+                        <h4>배달 </h4>
+                      </div>
+                      <div className="requestedof">
+                        <p>맛있게해주세요</p>
+                        <p>빨리와주세요</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="orderedMenu">
+                    <h3>주문내역</h3>
+                    <div className="orderedMenuInf">
+                      <div className="menuName">양념치킨</div>
+                      <div className="menuAmount">1</div>
+                      <div className="menuPrice">3,000</div>
+                    </div>
+                    <div className="orderedMenuInf">
+                      <div className="menuName">양념치킨</div>
+                      <div className="menuAmount">1</div>
+                      <div className="menuPrice">3,000</div>
+                    </div>
+                    <div className="orderedMenuInf">
+                      <div className="menuName">양념치킨</div>
+                      <div className="menuAmount">1</div>
+                      <div className="menuPrice">3,000</div>
+                    </div>
+                    <div className="allOrderedMenuInf">
+                      <div className="title">총주문</div>
+                      <div className="allMenuAmount">1</div>
+                      <div className="allMenuPrice">3,000</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="AnOrderRight">
+                  <div className="AnOrderInf">
+                    <div className="orderAddress">
+                      <h3> 배달주소</h3>
+                      <p>경기도 화성시 이성로 대명아파트 102동 906호</p>
+                    </div>
+                    <div className="orderCallNumber">
+                      <h3>고객연락처</h3>
+                      <p>010-1111-2222</p>
+                    </div>
+                    <div className="orderCallNumber">
+                      <h3>주문번호</h3>
+                      <p>PK012345143516</p>
+                    </div>
+                    <div className="orderCallNumber">
+                      <h3> 주문시간</h3>
+                      <p>13:54</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="acceptOrRefuse">
+                <button className="btn">접수하기</button>
+                <button className="btn">거절하기</button>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="todaySale"></div>
-    </div>
+        <div className="todaySale"></div>
+      </div>
+    </>
   );
 };
 
