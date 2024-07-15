248bdb2350b360910f3540bb1151be83bf231d53
miss-gif <ddc.akari@gmail.com>
2024-07-15 20:37:17 +0900

    Design : 확장 기능을 위한 컨텐츠 none 처리

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,20 +8,22 @@ const HomePage = () => {
   return (
     <div className="home-page">
       <CategoryNavigation />
-      <section className="section-carousel">
-        <h3 className="section__title">
-          <span>nickname</span>님을 위한 추천
-        </h3>
-        <RecommendationCarousel />
-      </section>
-      <section className="section-carousel">
-        <h3 className="section__title">새로 들어왔어요!</h3>
-        <NewRestaurantListCarousel />
-      </section>
-      <section className="section-carousel">
-        <h3 className="section__title">최근에 주문한 메뉴</h3>
-        <RecentOrderCarousel />
-      </section>
+      <div className="none">
+        <section className="section-carousel">
+          <h3 className="section__title">
+            <span>nickname</span>님을 위한 추천
+          </h3>
+          <RecommendationCarousel />
+        </section>
+        <section className="section-carousel">
+          <h3 className="section__title">새로 들어왔어요!</h3>
+          <NewRestaurantListCarousel />
+        </section>
+        <section className="section-carousel">
+          <h3 className="section__title">최근에 주문한 메뉴</h3>
+          <RecentOrderCarousel />
+        </section>
+      </div>
     </div>
   );
 };
