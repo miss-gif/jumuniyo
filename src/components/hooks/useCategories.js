89d73461c7d3d24b3e352b8f02f02fb5ab20d442
miss-gif <ddc.akari@gmail.com>
2024-07-15 20:35:38 +0900

    Feat : 카테고리 정보 불러오기

---
+++ src/components/hooks/useCategories.js
@@ -0,0 +1,28 @@
+// useCategories.js
+import { useState, useEffect } from "react";
+import { fetchCategories } from "../../api/user/category";
+
+const useCategories = () => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const data = await fetchCategories();
+        setCategories(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getCategories();
+  }, []);
+
+  return { categories, loading, error };
+};
+
+export default useCategories;
