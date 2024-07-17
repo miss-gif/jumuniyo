14d419d96d46fe723d1fdf8d1afbb25a535543df
miss-gif <ddc.akari@gmail.com>
2024-07-18 03:19:04 +0900

    Test : 결제 후 이동경로 수정

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -38,7 +38,7 @@ const PaymentPage = () => {
 
       if (res.data.statusCode === 1) {
         alert(res.data.resultMsg);
-        navigate(`/orderview/${res.data.resultData}`);
+        navigate(`/mypage/order/${res.data.resultData}`);
       } else {
         alert("결제에 실패했습니다. 다시 시도해주세요.");
       }
