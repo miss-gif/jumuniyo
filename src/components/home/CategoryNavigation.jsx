8c18fb399726abea5838ce3c7f79b3e450d5fbf4
miss-gif <ddc.akari@gmail.com>
2024-07-17 23:09:24 +0900

    Test : 카테고리 기능 구현 1. path 정리

--- src/components/home/CategoryNavigation.jsx
+++ src/components/home/CategoryNavigation.jsx
@@ -1,9 +1,28 @@
 // CategoryNavigation.js
 import React from "react";
+import PropTypes from "prop-types";
 import useCategories from "../../hooks/useCategories";
+import { useNavigate } from "react-router-dom";
+
+const CategoryItem = ({ categoryPk, categoryName, onClick }) => (
+  <li className="category__item border-set bc" onClick={onClick}>
+    <p>{categoryName}</p>
+  </li>
+);
+
+CategoryItem.propTypes = {
+  categoryPk: PropTypes.number.isRequired,
+  categoryName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 const CategoryNavigation = () => {
   const { categories, loading, error } = useCategories();
+  const navigate = useNavigate();
+
+  const onClickLink = categoryPk => () => {
+    navigate(`/restaurants/category_id=${categoryPk}&page=1&order_type=1`);
+  };
 
   if (loading) return <p>로딩 중...</p>;
   if (error) return <p>에러 발생: {error}</p>;
@@ -13,16 +32,18 @@ const CategoryNavigation = () => {
       <h2 className="hidden">카테고리 선택 메뉴</h2>
       <section className="category">
         <ul className="category__list">
-          <li className="category__item border-set bc">
-            <p>전체보기</p>
-          </li>
+          <CategoryItem
+            categoryPk={0}
+            categoryName="전체보기"
+            onClick={onClickLink(0)}
+          />
           {categories.map(category => (
-            <li
+            <CategoryItem
               key={category.categoryPk}
-              className="category__item border-set bc"
-            >
-              <p>{category.categoryName}</p>
-            </li>
+              categoryPk={category.categoryPk}
+              categoryName={category.categoryName}
+              onClick={onClickLink(category.categoryPk)}
+            />
           ))}
         </ul>
       </section>
