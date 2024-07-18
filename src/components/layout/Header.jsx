8d211143f417c66fec2ab6dc99940eda31fd42e5
miss-gif <svx327@naver.com>
2024-07-18 13:22:51 +0900

    Feat : 로그아웃 기능

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 import MenuIcon from "@mui/icons-material/Menu";
 import PersonIcon from "@mui/icons-material/Person";
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
 function Header() {
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies(["accessToken"]);
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/api/sign-out", {
+        headers: {
+          Authorization: `Bearer ${cookies.accessToken}`,
+        },
+      });
+
+      if (response.status === 200) {
+        // 쿠키에서 accessToken 삭제
+        removeCookie("accessToken", { path: "/" });
+
+        // 로그아웃 성공 시, 로그인 페이지로 리디렉션
+        navigate("/login");
+      } else {
+        // 에러 처리 (예: 에러 메시지 표시)
+        console.error("Logout failed");
+      }
+    } catch (error) {
+      console.error("An error occurred during logout", error);
+    }
+  };
+
   return (
     <header className="header">
       <div className="inner">
@@ -28,9 +56,6 @@ function Header() {
           </ul>
           <ul className="nav__list">
             <li className="nav__item">
-              <Link to="/login-test">log-in</Link>
-            </li>
-            <li className="nav__item">
               <Link to="/payment">결제하기</Link>
             </li>
             <li className="nav__item">
@@ -38,6 +63,9 @@ function Header() {
             </li>
             <li className="nav__item">
               <Link to="/mypage">마이페이지</Link>
+            </li>
+            <li className="nav__item">
+              <button onClick={handleLogout}>로그아웃</button>
             </li>
             <li className="nav__item nav__item--login">
               <Link to="/login">로그인</Link>
