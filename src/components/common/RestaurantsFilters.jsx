2f9668fb1834de0388fa0c3dc7510ecaac08833f
miss-gif <svx327@naver.com>
2024-07-19 15:46:26 +0900

    Feat : RestaurantsFilters 필터 기능

--- src/components/common/RestaurantsFilters.jsx
+++ src/components/common/RestaurantsFilters.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect, useRef } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import useCategories from "../../hooks/useCategories";
 import ModalMenuSearch from "../../pages/user/restaurantListPage/ModalMenuSearch.jsx";
+import { useNavigate } from "react-router-dom";
 
 const RestaurantsFilters = () => {
   const [isMenuSearchVisible, setMenuSearchVisible] = useState(false);
   const menuSearchRef = useRef(null);
 
   const { categories, loading, error } = useCategories();
+  const navigate = useNavigate();
 
   const onClickMenuSearch = () => {
     setMenuSearchVisible(!isMenuSearchVisible);
@@ -29,6 +31,10 @@ const RestaurantsFilters = () => {
     };
   }, []);
 
+  const onClickLink = categoryPk => () => {
+    navigate(`/restaurant/category_id=${categoryPk}`);
+  };
+
   if (loading) return <p>로딩 중...</p>;
   if (error) return <p>에러 발생: {error}</p>;
 
@@ -41,9 +47,15 @@ const RestaurantsFilters = () => {
               <SearchIcon />
             </button>
             <ul className="filters__list">
-              <li className="filters__item btn-active">전체보기</li>
+              <li className="filters__item btn-active" onClick={onClickLink(0)}>
+                전체보기
+              </li>
               {categories.map(category => (
-                <li key={category.categoryPk} className="filters__item">
+                <li
+                  key={category.categoryPk}
+                  className="filters__item"
+                  onClick={onClickLink(category.categoryPk)}
+                >
                   {category.categoryName}
                 </li>
               ))}
