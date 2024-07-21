244c4dd3f2c22e687c028817703d7cf06f131389
miss-gif <ddc.akari@gmail.com>
2024-07-21 11:42:33 +0900

    Rename : test파일 삭제 및 redux Persist 설정

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { register } from "swiper/element/bundle";
 import { Provider } from "react-redux";
-import store from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./app/store";
+import { register } from "swiper/element/bundle"; // Swiper 사용 시
 
 // Swiper 요소 등록
 register();
@@ -13,8 +14,10 @@ register();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 );
