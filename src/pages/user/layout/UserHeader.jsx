3470f6a5f4c42651c58499eeb15b3fbf03b98e7d
miss-gif <svx327@naver.com>
2024-08-01 17:50:09 +0900

    Test : 메뉴 사이드바 작업중

--- src/pages/user/layout/UserHeader.jsx
+++ src/pages/user/layout/UserHeader.jsx
@@ -1,16 +1,27 @@
+import React, { useState } from "react";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { IoCartOutline, IoSearchSharp } from "react-icons/io5";
-import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowDown, IoIosMenu } from "react-icons/io";
 import { Link } from "react-router-dom";
-import { GiHamburgerMenu } from "react-icons/gi";
 
 import "./UserHeader.scss";
 
 const UserHeader = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+    if (!isSidebarOpen) {
+      document.body.style.overflow = "hidden"; // 스크롤 정지
+    } else {
+      document.body.style.overflow = "auto"; // 스크롤 재개
+    }
+  };
+
   return (
     <div className="user-header">
-      <button className="user-header__menu-btn">
-        <GiHamburgerMenu fontSize={24} />
+      <button className="user-header__menu-btn" onClick={toggleSidebar}>
+        <IoIosMenu fontSize={32} />
       </button>
       <div className="user-header__logo">주문이요</div>
       <nav className="user-header__nav">
@@ -28,13 +39,22 @@ const UserHeader = () => {
         </div>
       </nav>
       <div className="user-header__auth">
-        <div className="user-header__auth-login">
+        <div className="user-header__auth-login auth-btn">
           <Link to="/login">로그인</Link>
         </div>
         <div className="user-header__auth-signup auth-btn">
           <Link to="/auth">회원가입</Link>
         </div>
       </div>
+
+      {isSidebarOpen && (
+        <div className="sidebar-overlay" onClick={toggleSidebar}>
+          <div className="sidebar" onClick={e => e.stopPropagation()}>
+            {/* 사이드바 내용 */}
+            <button onClick={toggleSidebar}>닫기</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
