7fcc149aa654ac413a3c6b30c1a79802597b4488
miss-gif <svx327@naver.com>
2024-08-01 19:53:43 +0900

    Test : 사이드바 트랜지션 효과

--- src/pages/user/layout/UserHeader.jsx
+++ src/pages/user/layout/UserHeader.jsx
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { IoCartOutline, IoSearchSharp } from "react-icons/io5";
 import { IoIosArrowDown, IoIosMenu } from "react-icons/io";
 import { Link } from "react-router-dom";
-
 import "./UserHeader.scss";
 
 const UserHeader = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isTransitioning, setTransitioning] = useState(false);
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      setTransitioning(true);
+      document.documentElement.style.overflow = "hidden";
+    } else if (isTransitioning) {
+      // 사이드바가 닫힐 때 애니메이션이 완료된 후 overflow를 auto로 되돌립니다.
+      const timer = setTimeout(() => {
+        document.documentElement.style.overflow = "auto";
+        setTransitioning(false);
+      }, 300); // 애니메이션 시간과 맞추어야 합니다.
+      return () => clearTimeout(timer);
+    }
+  }, [isSidebarOpen, isTransitioning]);
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
-    if (!isSidebarOpen) {
-      document.documentElement.style.overflow = "hidden"; // 스크롤 정지
-    } else {
-      document.documentElement.style.overflow = "auto"; // 스크롤 재개
-    }
   };
 
   return (
@@ -23,7 +32,9 @@ const UserHeader = () => {
       <button className="user-header__menu-btn" onClick={toggleSidebar}>
         <IoIosMenu fontSize={32} />
       </button>
-      <div className="user-header__logo">주문이요</div>
+      <div className="user-header__logo">
+        <a href="/">주문이요</a>
+      </div>
       <nav className="user-header__nav">
         <div className="user-header__location">
           <MdOutlineLocationOn />
@@ -46,15 +57,18 @@ const UserHeader = () => {
           <Link to="/auth">회원가입</Link>
         </div>
       </div>
-
-      {isSidebarOpen && (
-        <div className="sidebar-overlay" onClick={toggleSidebar}>
-          <div className="sidebar" onClick={e => e.stopPropagation()}>
-            {/* 사이드바 내용 */}
-            <button onClick={toggleSidebar}>닫기</button>
-          </div>
+      <div
+        className={`sidebar-overlay ${isSidebarOpen ? "visible" : ""}`}
+        onClick={toggleSidebar}
+      >
+        <div
+          className={`sidebar ${isSidebarOpen ? "open" : ""}`}
+          onClick={e => e.stopPropagation()}
+        >
+          <div>회원가입</div>
+          <div>로그인</div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
