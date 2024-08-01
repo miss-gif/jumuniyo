1936a190b2520d08556ec165dabb5d3de9bc8fd5
miss-gif <svx327@naver.com>
2024-08-01 17:06:01 +0900

    Test : 새로운 헤더 작업중

--- src/pages/user/layout/UserHeader.jsx
+++ src/pages/user/layout/UserHeader.jsx
@@ -1,30 +1,38 @@
 import { MdOutlineLocationOn } from "react-icons/md";
 import { IoCartOutline, IoSearchSharp } from "react-icons/io5";
-import "./UserHeader.scss";
 import { IoIosArrowDown } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import "./UserHeader.scss";
 
 const UserHeader = () => {
   return (
     <div className="user-header">
-      <div className="inner">
-        <div className="user-header__logo">주문이요</div>
-        <nav className="user-header__nav">
-          <div className="user-header__location">
-            <MdOutlineLocationOn />
-            <span>대구 중구</span>
-            <IoIosArrowDown />
-          </div>
-          <div className="user-header__search">
-            <IoSearchSharp />
-            <input className="search__input" type="text" value="검색창" />
-          </div>
-          <div className="user-header__cart">
-            <IoCartOutline fontSize={24} />
-          </div>
-        </nav>
-        <div className="user-header__auth">
-          <div className="user-header__auth-login auth-btn">로그인</div>
-          <div className="user-header__auth-signup auth-btn">회원가입</div>
+      <button className="user-header__menu-btn">
+        <GiHamburgerMenu fontSize={24} />
+      </button>
+      <div className="user-header__logo">주문이요</div>
+      <nav className="user-header__nav">
+        <div className="user-header__location">
+          <MdOutlineLocationOn />
+          <span>대구 중구</span>
+          <IoIosArrowDown />
+        </div>
+        <div className="user-header__search">
+          <IoSearchSharp />
+          <input className="search__input" type="text" placeholder="검색창" />
+        </div>
+        <div className="user-header__cart">
+          <IoCartOutline fontSize={24} />
+        </div>
+      </nav>
+      <div className="user-header__auth">
+        <div className="user-header__auth-login">
+          <Link to="/login">로그인</Link>
+        </div>
+        <div className="user-header__auth-signup auth-btn">
+          <Link to="/auth">회원가입</Link>
         </div>
       </div>
     </div>
