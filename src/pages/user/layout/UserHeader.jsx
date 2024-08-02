df13c95b5a8016123203bfbf50d68b9f49b40cd8
miss-gif <svx327@naver.com>
2024-08-02 15:32:08 +0900

    Fix : 모달 열렸을 때, 스크롤 제어

--- src/pages/user/layout/UserHeader.jsx
+++ src/pages/user/layout/UserHeader.jsx
@@ -62,13 +62,13 @@ const UserHeader = () => {
   const openModal = item => {
     setSelectedItem(item);
     setIsModal(true);
-    document.body.style.overflow = "hidden"; // 스크롤 정지
+    document.documentElement.style.overflow = "hidden";
   };
 
   const closeModal = () => {
     setIsModal(false);
     setSelectedItem(null);
-    document.body.style.overflow = "auto"; // 스크롤 해제
+    document.documentElement.style.overflow = "auto";
   };
 
   return (
