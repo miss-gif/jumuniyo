660560fd2f61258c5b72274fc0fd1e745a48f95e
miss-gif <ddc.akari@gmail.com>
2024-08-09 07:10:38 +0900

    Test : GoogleMaps 하드리셋

--- src/components/common/GoogleMaps.jsx
+++ src/components/common/GoogleMaps.jsx
@@ -9,8 +9,6 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import parse from "autosuggest-highlight/parse";
 import { debounce } from "@mui/material/utils";
-import { useDispatch, useSelector } from "react-redux";
-import { setLocationData } from "../../app/userSlice";
 
 const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
@@ -27,88 +25,24 @@ function loadScript(src, position, id) {
 }
 
 const autocompleteService = { current: null };
-const placesService = { current: null };
-const geocoderService = { current: null };
 
-const GoogleMaps = () => {
-  const dispatch = useDispatch();
-  const locationData = useSelector(state => state.user.locationData);
-  const searchTerm = useSelector(state => state.user.searchTerm);
-
+export default function GoogleMaps() {
   const [value, setValue] = useState(null);
   const [inputValue, setInputValue] = useState("");
   const [options, setOptions] = useState([]);
   const loaded = useRef(false);
 
-  useEffect(() => {
-    if (typeof window !== "undefined" && !loaded.current) {
-      if (!document.querySelector("#google-maps")) {
-        loadScript(
-          `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`,
-          document.querySelector("head"),
-          "google-maps",
-        );
-      }
-      loaded.current = true;
+  if (typeof window !== "undefined" && !loaded.current) {
+    if (!document.querySelector("#google-maps")) {
+      loadScript(
+        `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`,
+        document.querySelector("head"),
+        "google-maps",
+      );
     }
-  }, []);
 
-  useEffect(() => {
-    // 위도와 경도가 모두 제공된 경우에만 실행
-    if (locationData.latitude && locationData.longitude) {
-      // 지오코더를 초기화하는 함수
-      const initializeGeocoder = () => {
-        // 지오코더 서비스가 아직 초기화되지 않았고, Google Maps API가 로드된 경우에만 실행
-        if (!geocoderService.current && window.google) {
-          geocoderService.current = new window.google.maps.Geocoder();
-        }
-
-        // 전달된 위도와 경도를 float 타입으로 변환하여 객체로 생성
-        const latlng = {
-          lat: parseFloat(locationData.latitude),
-          lng: parseFloat(locationData.longitude),
-        };
-
-        // 지오코더 서비스가 초기화된 경우, 지오코딩 요청을 실행
-        if (geocoderService.current) {
-          geocoderService.current.geocode(
-            { location: latlng },
-            (results, status) => {
-              // 요청이 성공적이고 결과가 있는 경우
-              if (status === "OK" && results[0]) {
-                setValue(results[0]); // 결과를 상태에 저장
-                setInputValue(results[0].formatted_address); // 포맷된 주소를 입력 값에 저장
-
-                // 리덕스에 지오코딩 주소 값을 저장
-                dispatch(
-                  setLocationData({
-                    latitude: latlng.lat,
-                    longitude: latlng.lng,
-                    geocodeAddress: results[0].formatted_address,
-                  }),
-                );
-              } else {
-                // 지오코딩 실패 시 콘솔에 에러 메시지 출력
-                console.error("지오코딩에 실패했습니다.");
-              }
-            },
-          );
-        }
-      };
-
-      // Google Maps API가 로드된 경우에만 지오코더 초기화
-      if (window.google) {
-        initializeGeocoder();
-      } else {
-        // Google Maps API가 아직 로드되지 않았을 때 에러 메시지 출력
-        console.error("Google Maps API가 아직 로드되지 않았습니다.");
-      }
-    }
-  }, [locationData.latitude, locationData.longitude, dispatch]); // 의존성 배열에 있는 값이 변경될 때마다 이 효과 실행
-
-  useEffect(() => {
-    setValue(searchTerm);
-  }, [value]);
+    loaded.current = true;
+  }
 
   const fetch = useMemo(
     () =>
@@ -125,14 +59,7 @@ const GoogleMaps = () => {
       autocompleteService.current =
         new window.google.maps.places.AutocompleteService();
     }
-
-    if (!placesService.current && window.google) {
-      placesService.current = new window.google.maps.places.PlacesService(
-        document.createElement("div"),
-      );
-    }
-
-    if (!autocompleteService.current || !placesService.current) {
+    if (!autocompleteService.current) {
       return undefined;
     }
 
@@ -162,41 +89,11 @@ const GoogleMaps = () => {
     };
   }, [value, inputValue, fetch]);
 
-  const handlePlaceSelect = (event, newValue) => {
-    setOptions(newValue ? [newValue, ...options] : options);
-    setValue(newValue);
-
-    if (newValue && newValue.place_id && placesService.current) {
-      placesService.current.getDetails(
-        { placeId: newValue.place_id },
-        (place, status) => {
-          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-            const latitude = place.geometry.location.lat();
-            const longitude = place.geometry.location.lng();
-            console.log("Latitude:", latitude);
-            console.log("Longitude:", longitude);
-
-            // 리덕스에 위치 데이터와 지오코딩 주소 값 저장
-            dispatch(
-              setLocationData({
-                latitude,
-                longitude,
-                geocodeAddress: place.formatted_address, // 지오코딩 주소 저장
-              }),
-            );
-          }
-        },
-      );
-    }
-  };
-
   return (
     <Autocomplete
       sx={{ width: "100%" }}
       getOptionLabel={option =>
-        typeof option === "string"
-          ? option
-          : option.description || option.formatted_address
+        typeof option === "string" ? option : option.description
       }
       filterOptions={x => x}
       options={options}
@@ -205,7 +102,10 @@ const GoogleMaps = () => {
       filterSelectedOptions
       value={value}
       noOptionsText="건물명, 도로명, 지번으로 검색하세요."
-      onChange={handlePlaceSelect}
+      onChange={(event, newValue) => {
+        setOptions(newValue ? [newValue, ...options] : options);
+        setValue(newValue);
+      }}
       onInputChange={(event, newInputValue) => {
         setInputValue(newInputValue);
       }}
@@ -231,12 +131,12 @@ const GoogleMaps = () => {
       renderOption={(props, option) => {
         const { key, ...optionProps } = props;
         const matches =
-          option.structured_formatting?.main_text_matched_substrings || [];
+          option.structured_formatting.main_text_matched_substrings || [];
+
         const parts = parse(
-          option.structured_formatting?.main_text || "",
+          option.structured_formatting.main_text,
           matches.map(match => [match.offset, match.offset + match.length]),
         );
-
         return (
           <li key={key} {...optionProps}>
             <Grid container sx={{ alignItems: "center" }}>
@@ -257,7 +157,7 @@ const GoogleMaps = () => {
                   </Box>
                 ))}
                 <Typography variant="body2" color="text.secondary">
-                  {option.structured_formatting?.secondary_text}
+                  {option.structured_formatting.secondary_text}
                 </Typography>
               </Grid>
             </Grid>
@@ -266,6 +166,4 @@ const GoogleMaps = () => {
       }}
     />
   );
-};
-
-export default GoogleMaps;
+}
