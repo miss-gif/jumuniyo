3c89d6f80ce496b629659e837a4561bba59c8011
miss-gif <ddc.akari@gmail.com>
2024-08-12 21:02:33 +0900

    Add : 쿠폰 이벤트 진행중인 상점 리스트

--- src/pages/user/main/index.jsx
+++ src/pages/user/main/index.jsx
@@ -1,13 +1,38 @@
-import React, { useRef } from "react";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 import { IoIosHeartEmpty } from "react-icons/io";
+import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
 import "swiper/css";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./MainPage.scss";
-import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 
 const MainPage = () => {
   const swiperRef = useRef(null);
+  const dispatch = useDispatch();
+  const { locationData } = useSelector(state => state.user);
+  const [coupons, setCoupons] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoupons = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          `/api/restaurant/coupon?addrX=${locationData.latitude}&addrY=${locationData.longitude}`,
+        );
+        setCoupons(response.data.resultData.list);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoupons();
+  }, [locationData]);
 
   const handlePrev = () => {
     if (swiperRef.current && swiperRef.current.swiper) {
@@ -23,9 +48,9 @@ const MainPage = () => {
 
   return (
     <div className="main-page">
-      <div className="슬라이드1">
+      <div className="coupon-event">
         <div className="carousel-header">
-          <h3 className="carousel-header__title">치킨</h3>
+          <h3 className="carousel-header__title">쿠폰 이벤트 진행중</h3>
           <div className="carousel-header__controller">
             <div className="carousel-header__controller-all">See all</div>
             <div className="carousel-header__controller-btns">
@@ -44,119 +69,54 @@ const MainPage = () => {
             </div>
           </div>
         </div>
-        <Swiper
-          ref={swiperRef}
-          slidesPerView={4}
-          spaceBetween={10}
-          modules={[Navigation]}
-          className="mySwiper main-page__list"
-        >
-          <SwiperSlide className="main-page__item">
-            <div className="main-page__image main-page__image--background">
-              <div className="main-page__toggle-heart">
-                <IoIosHeartEmpty />
-              </div>
-            </div>
-            <div className="main-page__store-info">
-              <div className="main-page__store-name">McDonald</div>
-              <div className="main-page__score">4.5</div>
-            </div>
-            <div className="main-page__review-info">
-              <div className="main-page__review">
-                리뷰
-                <span className="main-page__review-count">33</span>
-              </div>
-              <div className="main-page__owner-review">
-                사장님리뷰
-                <span className="main-page__owner-review-count">33</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="main-page__item">
-            <div className="main-page__image main-page__image--background">
-              <div className="main-page__toggle-heart">
-                <IoIosHeartEmpty />
-              </div>
-            </div>
-            <div className="main-page__store-info">
-              <div className="main-page__store-name">McDonald</div>
-              <div className="main-page__score">4.5</div>
-            </div>
-            <div className="main-page__review-info">
-              <div className="main-page__review">
-                리뷰
-                <span className="main-page__review-count">33</span>
-              </div>
-              <div className="main-page__owner-review">
-                사장님리뷰
-                <span className="main-page__owner-review-count">33</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="main-page__item">
-            <div className="main-page__image main-page__image--background">
-              <div className="main-page__toggle-heart">
-                <IoIosHeartEmpty />
-              </div>
-            </div>
-            <div className="main-page__store-info">
-              <div className="main-page__store-name">McDonald</div>
-              <div className="main-page__score">4.5</div>
-            </div>
-            <div className="main-page__review-info">
-              <div className="main-page__review">
-                리뷰
-                <span className="main-page__review-count">33</span>
-              </div>
-              <div className="main-page__owner-review">
-                사장님리뷰
-                <span className="main-page__owner-review-count">33</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="main-page__item">
-            <div className="main-page__image main-page__image--background">
-              <div className="main-page__toggle-heart">
-                <IoIosHeartEmpty />
-              </div>
-            </div>
-            <div className="main-page__store-info">
-              <div className="main-page__store-name">McDonald</div>
-              <div className="main-page__score">4.5</div>
-            </div>
-            <div className="main-page__review-info">
-              <div className="main-page__review">
-                리뷰
-                <span className="main-page__review-count">33</span>
-              </div>
-              <div className="main-page__owner-review">
-                사장님리뷰
-                <span className="main-page__owner-review-count">33</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="main-page__item">
-            <div className="main-page__image main-page__image--background">
-              <div className="main-page__toggle-heart">
-                <IoIosHeartEmpty />
-              </div>
-            </div>
-            <div className="main-page__store-info">
-              <div className="main-page__store-name">McDonald</div>
-              <div className="main-page__score">4.5</div>
-            </div>
-            <div className="main-page__review-info">
-              <div className="main-page__review">
-                리뷰
-                <span className="main-page__review-count">33</span>
-              </div>
-              <div className="main-page__owner-review">
-                사장님리뷰
-                <span className="main-page__owner-review-count">33</span>
-              </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Error: {error}</div>
+        ) : (
+          <Swiper
+            ref={swiperRef}
+            slidesPerView={4}
+            spaceBetween={10}
+            modules={[Navigation]}
+            className="mySwiper main-page__list"
+          >
+            {coupons.map(coupon => (
+              <SwiperSlide
+                key={coupon.restaurantPk}
+                className="main-page__item"
+              >
+                <div className="main-page__image main-page__image--background">
+                  <div className="main-page__store-img__cover">
+                    <img
+                      src={`/pic/${coupon.restaurantPic}`}
+                      alt={coupon.restaurantName}
+                    />
+                  </div>
+                  <div className="main-page__toggle-heart">
+                    <IoIosHeartEmpty />
+                  </div>
+                </div>
+                <div className="main-page__store-info">
+                  <div className="main-page__store-name">
+                    {coupon.restaurantName}
+                  </div>
+                  <div className="main-page__score">
+                    {coupon.reviewAvgScore.toFixed(1)}
+                  </div>
+                </div>
+                <div className="main-page__review-info">
+                  <div className="main-page__review">
+                    리뷰
+                    <span className="main-page__review-count">
+                      {coupon.reviewTotalElements}
+                    </span>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </div>
   );
