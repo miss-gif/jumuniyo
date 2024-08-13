8cac4c64c33da94a97cd5e86a7e78a525d940732
miss-gif <ddc.akari@gmail.com>
2024-08-13 19:23:19 +0900

    Test : 카테고리 코드 정리

--- src/hooks/useCategories.js
+++ src/hooks/useCategories.js
@@ -1,24 +1,25 @@
 // useCategories.js
-import { useState, useEffect } from "react";
-import { fetchCategories } from "../api/user/category";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const useCategories = () => {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const getCategories = async () => {
-      try {
-        const data = await fetchCategories();
-        setCategories(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const getCategories = async () => {
+    const API_URL = "/api/restaurant/category";
+    try {
+      const data = await axios.get(API_URL);
+      setCategories(data.data.resultData);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     getCategories();
   }, []);
 
