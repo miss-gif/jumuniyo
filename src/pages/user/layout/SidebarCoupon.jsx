27a9d1c05c78f9c6b0fe7c123b3964b1f608dd7d
miss-gif <svx327@naver.com>
2024-08-22 17:40:02 +0900

    Fix : 사이드바 쿠폰과 사이드바 하트 컴포넌트에 대한 변경 내용 추가

--- src/pages/user/layout/SidebarCoupon.jsx
+++ src/pages/user/layout/SidebarCoupon.jsx
@@ -47,7 +47,7 @@ const SidebarCoupon = ({ isSidebarCoupon, toggleSidebarCoupon }) => {
     if (isLoggedIn) {
       fetchCoupons();
     }
-  }, [isLoggedIn, accessToken]);
+  }, [isLoggedIn, accessToken, coupons]);
 
   return (
     <div
